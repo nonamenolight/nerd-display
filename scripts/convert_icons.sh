801a9904b2c0d89d3c7602dd4c7fc1bb4a2ef05b
nonamenolight <w1471725096@gmail.com>
2026-08-07 23:04:19 +0800

    fix bugs

--- scripts/convert_icons.sh
+++ scripts/convert_icons.sh
@@ -5,7 +5,7 @@ set -e
 SRC_DIR="assets/icons/svg"
 DST_DIR="assets/icons/png"
 
-SIZE=512
+HEIGHT=512
 
 mkdir -p "$DST_DIR"
 
@@ -13,13 +13,21 @@ for svg in "$SRC_DIR"/*.svg
 do
     filename=$(basename "$svg" .svg)
 
-    echo "Converting $filename..."
+    # 转换文件名:
+    # 空格 -> -
+    # 大写 -> 小写
+    # 连续特殊字符处理
+    output=$(echo "$filename" \
+        | sed 's/ /-/g' \
+        | tr '[:upper:]' '[:lower:]')
+
+    echo "Converting $filename.svg -> $output.png"
 
     rsvg-convert \
-        -w $SIZE \
-        -h $SIZE \
+        -h $HEIGHT \
         "$svg" \
-        -o "$DST_DIR/$filename.png"
+        -o "$DST_DIR/$output.png"
+
 done
 
 echo "Done!"
